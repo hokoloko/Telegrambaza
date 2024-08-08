d3b39a0fa5b15c895965ae4d8cdcefee1b65f804
hokoloko <119777388+hokoloko@users.noreply.github.com>
2024-08-08 11:13:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-// Import necessary modules
+
 const TelegramBot = require('node-telegram-bot-api');
 
-// Replace 'YOUR_BOT_TOKEN' with the token you received from BotFather
 const token = '7058348107:AAHi1ots6aamj_fYrQhH5MoiaWce4w74Q6M';
 const bot = new TelegramBot(token, { polling: true });
 
-// Command: /start
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
     bot.sendMessage(chatId, 'Welcome to your Telegram Bot! Type /menu to see the menu.');
 });
 
-// Command: /menu
 bot.onText(/\/menu/, (msg) => {
     const chatId = msg.chat.id;
     const menuOptions = {
@@ -25,13 +22,11 @@ bot.onText(/\/menu/, (msg) => {
     bot.sendMessage(chatId, 'Choose an option:', menuOptions);
 });
 
-// Command: /hello
 bot.onText(/\/hello/, (msg) => {
     const chatId = msg.chat.id;
     bot.sendMessage(chatId, 'Hello! 👋');
 });
 
-// Command: /goodbye
 bot.onText(/\/goodbye/, (msg) => {
     const chatId = msg.chat.id;
     bot.sendMessage(chatId, 'Goodbye! 👋');
